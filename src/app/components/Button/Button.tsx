220467f3a8a4be83a3055f1a461d3f036ab43135
Patrick Möller <patrick_moeller@live.de>
2021-11-09 16:17:47 +0100

    Reinstall styled_components

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react';
 import React from 'react';
 import styled from 'styled-components';
-import classes from './Button.module.css';
 
 type ButtonProps = {
   children: ReactNode;
